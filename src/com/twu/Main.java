6b26dda525750e8355ad7d9b8431ac9331a2fb2c
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-11 00:03:15 +0800

    [pyqpyqpyq] feat: java-basic-practice

--- src/com/twu/Main.java
+++ src/com/twu/Main.java
@@ -1,7 +1,98 @@
-package com.twu;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
+        init();
+    }
+//    public class record{
+//        public String[][] record= new String[][];
+//    }
+
+    public static void init(){
+        greet();
+        user_choose();
+    }
+
+    public static void greet(){
+        System.out.println("欢迎来到热搜榜，你是？");
+        System.out.println("1.用户");
+        System.out.println("2.管理员");
+        System.out.println("3.退出");
+    }
+
+    public static void user_choose(){
+        Scanner input= new Scanner(System.in);
+        int user = input.nextInt();
+        if(user == 1) {
+            switch_to_user();
+        }else if(user == 2){
+            switch_to_admin();
+        }
+    }
+
+    public static void switch_to_user(){
+        System.out.println("请输入您的昵称");
+        Scanner input= new Scanner(System.in);
+        String username = input.nextLine();
+        System.out.print("你好，");
+        System.out.print(username);
+        System.out.println("，你可以");
+        System.out.println("1.查看热搜排行榜");
+        System.out.println("2.给热搜事件投票");
+        System.out.println("3.购买热搜");
+        System.out.println("4.添加热搜");
+        System.out.println("5.退出");
+        int choice = input.nextInt();
+        switch (choice){
+            case 1: show_hot_search();
+            case 2: vote_hotsearch();
+            case 3: buy_hotsearch();
+            case 4: add_hotsearch();
+            default: init();
+        }
+}
+    public static void switch_to_admin(){
+        System.out.println("请输入您的昵称");
+        Scanner input= new Scanner(System.in);
+        String username = input.nextLine();
+        System.out.println("请输入您的密码");
+        String pwd = input.nextLine();
+        admin(username);
+    }
+
+    public static void show_hot_search(){
+
+    }
+
+    public static void vote_hotsearch(){
+
+    }
+
+    public static void buy_hotsearch(){
+
+    }
+    public static void add_hotsearch(){
+
+    }
+
+    public static void add_super_hotsearch(){
+
+    }
+    public static void admin(String username){
+        System.out.println("你好，");
+        System.out.println(username);
+        System.out.println(",你可以");
+        System.out.println("1.查看热搜排行榜");
+        System.out.println("2.添加热搜");
+        System.out.println("3.添加超级热搜");
+        System.out.println("4.退出");
+        Scanner input= new Scanner(System.in);
+        int choice = input.nextInt();
+        switch (choice){
+            case 1:show_hot_search();
+            case 2:add_hotsearch();
+            case 3:add_super_hotsearch();
+            default:init();
+        }
     }
 }
