974478d0b11daafe7cb96835045e0849be7663f7
liuchanglei <uestclcl@163.com>
2020-09-12 22:27:00 +0800

    增加按排名显示热搜的逻辑

--- src/com/twu/hot_search.java
+++ src/com/twu/hot_search.java
@@ -1,5 +1,5 @@
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class hot_search {
 
@@ -8,16 +8,16 @@ public class hot_search {
     private boolean if_super;
 
     public hot_search(String name, int times) {
-        this.name=name;
-        this.times=times;
-        this.if_super=false;
+        this.name = name;
+        this.times = times;
+        this.if_super = false;
     }
 
     public hot_search(String name, int times, boolean if_super) {
-        this.name=name;
-        this.times=times;
-        this.if_super=if_super;
-}
+        this.name = name;
+        this.times = times;
+        this.if_super = if_super;
+    }
 
     public void setTimes(int times) {
         this.times = times;
@@ -30,6 +30,7 @@ public class hot_search {
     public int getTimes() {
         return times;
     }
+
     public void addTimes() {
         times++;
     }
@@ -45,7 +46,19 @@ public class hot_search {
 
     public static void show_hot_search(People people) {
 //       show list
-        List<hot_search> hs = hot_search_list
+        Map<String, hot_search> hs = Main.hot_search_list;
+        Set<Map.Entry<String, hot_search>> hsEntry = hs.entrySet();
+        if (!hsEntry.isEmpty()) {
+            List<hot_search> rankHs = hsEntry.stream()
+                    .sorted((o1, o2) -> o2.getValue().getTimes() - o1.getValue().getTimes())
+                    .map(Map.Entry::getValue)
+                    .collect(Collectors.toList());
+            for (hot_search item : rankHs) {
+                System.out.print(item.getName());
+                System.out.print("  ");
+                System.out.println(item.getTimes());
+            }
+        }
         System.out.println("请输入您的昵称");
         Admin.Admin_choice(people);
     }
@@ -66,11 +79,11 @@ public class hot_search {
         if (Main.hot_search_list.containsKey(hot_search)) {
             Main.hot_search_list.get(hot_search).addTimes();
         } else {
-            hot_search hs = new hot_search(hot_search,1);
+            hot_search hs = new hot_search(hot_search, 1);
         }
-        if (people instanceof Admin){
+        if (people instanceof Admin) {
             User.User_choice(people);
-        }else {
+        } else {
             Admin.Admin_choice(people);
         }
 
